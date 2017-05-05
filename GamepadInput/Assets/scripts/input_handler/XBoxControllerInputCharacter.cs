efc926a90f4692cbce53167b9ece3c8931332406
Camilo Romero <camiloromero@Camilos-MacBook-Pro.local>
2017-05-04 19:22:58 -0500

    Fixed Control to Character

---
+++ GamepadInput/Assets/scripts/input_handler/XBoxControllerInputCharacter.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XBoxControllerInputCharacter: MonoBehaviour
+{
+	[SerializeField]
+	private float _umbral = 0.3f;
+
+	[SerializeField]
+	private bool _isListening = true;
+
+	[SerializeField]
+	private float _coolDownTime = 0.5f;
+
+	[SerializeField]
+	private CharacterPreviewController _character;
+ 
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetAxis("A Button") == 1)
+		{
+			ToggleListening(false, 0); 
+			 
+			_character.Attack();
+			 
+
+			StartCoroutine(ToggleListening(true, _coolDownTime));
+
+		}
+		if (Input.GetAxis("B Button") == 1)
+		{
+			ToggleListening(false, 0); 
+			 
+			_character.Idle();
+			 
+
+			StartCoroutine(ToggleListening(true, _coolDownTime));
+
+		}
+		if (Input.GetAxis("X Button") == 1)
+		{
+			ToggleListening(false, 0); 
+			 
+			_character.Run();
+			 
+
+			StartCoroutine(ToggleListening(true, _coolDownTime));
+
+		}
+
+	}
+
+	IEnumerator ToggleListening(bool status, float delay)
+	{
+		if (status)
+		{
+			yield return new WaitForSeconds(delay);
+		}
+		_isListening = status;
+	}
+}
+
+ 
